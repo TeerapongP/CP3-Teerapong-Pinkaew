b01684896facfdfa1ac20358a2fc65c21a43efe0
TeerapongP <56886497+TeerapongP@users.noreply.github.com>
2019-10-22 23:43:04 +0700

    Add files via upload

---
+++ Exercise 4 .py
@@ -0,0 +1,11 @@
+English = 80
+Business = 70
+ComputerSystems = 82.5
+ComputerProgramming = 96.18
+print("-----You score-----")
+print("Foundation English :",English)
+print("General Business :",Business)
+print("Introduction to Computer Systems :",ComputerSystems)
+print("Computer Programming :",ComputerProgramming)
+
+
