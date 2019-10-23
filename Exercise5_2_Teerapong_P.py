a41f0b3929e5db32449f419de47ff2417952d8f7
TeerapongP <56886497+TeerapongP@users.noreply.github.com>
2019-10-23 15:41:24 +0700

    Add files via upload

---
+++ Exercise5_2_Teerapong_P.py
@@ -0,0 +1,8 @@
+'''
+v คืออัตราเร็วของการเคลื่อนที่ (m/s หรือ km/hr)
+t คือเวลาที่วัตถุใช้เคลื่อนที่จริง (s หรือ hr)
+'''
+s = int(input("How Distance :  \n "))
+t = float(input("How long : \n"))
+v = s/t
+print(v,"km/h")
