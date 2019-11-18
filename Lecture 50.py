ccfcf3a517b16455d4b5e5d89ecf38ebf5def11c
TeerapongP <56886497+TeerapongP@users.noreply.github.com>
2019-11-18 13:36:24 +0700

    Add files via upload

---
+++ Lecture 50.py
@@ -0,0 +1,14 @@
+inputNumber = int(input("Enter number  1 : "))
+inputNumber2 = int(input("Enter number 2 : "))
+def adnumberplus(inputNumber,inputNumber2):
+  print(inputNumber+inputNumber2)
+adnumberplus(inputNumber,inputNumber2)
+def adnumberminus(inputNumber,inputNumber2):
+  print(inputNumber-inputNumber2)
+adnumberminus(inputNumber,inputNumber2)
+def adnumbermultiply(inputNumber,inputNumber2):
+  print(inputNumber*inputNumber2)
+adnumbermultiply(inputNumber,inputNumber2)
+def adnumberdivide(inputNumber,inputNumber2):
+  print(inputNumber/inputNumber2)
+adnumberdivide(inputNumber,inputNumber2)
